b20ba21d7e842cfb68b7f9ef40513a03ba950f5a
LucaZedda <58110072+Snarci@users.noreply.github.com>
2022-08-18 12:39:51 +0200

    Aggiunta lettura luce

--- server/server.jsx
+++ server/server.jsx
@@ -1,13 +1,13 @@
 const express = require('express');
 const awsIot = require('aws-iot-device-sdk');
 
-const name = 'ESP32';
+const name = 'ESP32_Meteo';
 const  thingShadows = awsIot.thingShadow({
     keyPath: './devices/esp32/privateKey.key',
     certPath: './devices/esp32/deviceCert.crt',
     caPath: './devices/esp32/AmazonRootCA1.pem',
-    clientId: 'Esp32', //nome policy
-    host: 'a2vvq6j3b7haes-ats.iot.us-east-1.amazonaws.com', //endpoint dispositivo
+    clientId: 'Meteo1', //nome policy
+    host: 'a8bjo0oi9t9yw-ats.iot.ap-south-1.amazonaws.com', //endpoint dispositivo
 })
 
 var humidityState = null;
@@ -22,8 +22,8 @@ app.use(express.json());
 thingShadows.on('connect', function() {
     thingShadows.register(name, {}, function() {
        thingShadows.get(name);
-       thingShadows.subscribe('esp32/pub');
-       thingShadows.subscribe('$aws/things/ESP32/shadow/update/documents');
+       //thingShadows.subscribe('esp32/pub');
+       thingShadows.subscribe('$aws/things/ESP32_Meteo/shadow/update/documents');
     });
 });
 
@@ -34,8 +34,10 @@ thingShadows.on('message', function(topic, payload) {
         var obj = JSON.parse(payload.toString());
         humidityState = obj.current.state.reported.value.humidity;
         temperatureState = obj.current.state.reported.value.temperature;
-        console.log("new Humidity: " + humidityState  + " new Temperature: " + temperatureState);
-        db
+        lightState = obj.current.state.reported.value.light;
+        console.log("new Humidity: " + humidityState  + " new Temperature: " + temperatureState +
+          " new Light: " + lightState);
+        /*db
         .collection(name)
         .insertOne({
             humidity: humidityState,
@@ -45,6 +47,7 @@ thingShadows.on('message', function(topic, payload) {
         })
         .then(result => console.log(result))
         .catch(err => console.error(err));
+        */
     }else{
       console.log('non ci interessa');
     }
@@ -55,6 +58,7 @@ thingShadows.on('status',  function(name, stat, clientToken,  stateObject) {
   console.log('received '+stat+' on '+name+': '+ JSON.stringify(stateObject));
   humidityState = stateObject.state.reported.value.humidity;
   temperatureState = stateObject.state.reported.value.temperature;
+  lightState = stateObject.state.reported.value.light;
 });
 
 const port = process.env.PORT || 5000;
