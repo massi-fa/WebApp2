f4095acfc9235cb850866cb622a545f86325cf0e
LucaZedda <58110072+Snarci@users.noreply.github.com>
2022-09-07 00:27:59 +0200

    Lettura messaggi shadow per ogni thing di cui si posseggono i certificati

--- server/server.jsx
+++ server/server.jsx
@@ -9,18 +9,19 @@ const { IoTDataPlaneClient, UpdateThingShadowCommand, GetThingShadowCommand, Pub
 const awsIot = require('aws-iot-device-sdk');
 const connectDB = require('./config/db');
 const { query } = require('express');
+const secrets = require('./config/secrets.json');
 const fs = require('fs');
 const config = {
   region: 'ap-south-1',
   credentials: {
-    accessKeyId: '',
-    secretAccessKey: '',
+    accessKeyId: secrets.accessKeyId,
+    secretAccessKey: secrets.secretAccessKey,
   },
 };
 const client = new IoTClient(config);
 const clientShadows = new IoTDataPlaneClient(config);
 const name = 'ESP32_Meteo';
-const  thingShadows = awsIot.thingShadow({
+/*const  thingShadows = awsIot.thingShadow({
     keyPath: './devices/esp32/privateKey.key',
     certPath: './devices/esp32/deviceCert.crt',
     caPath: './devices/esp32/AmazonRootCA1.pem',
@@ -32,7 +33,7 @@ console.log(thingShadows);
 var humidityState = null;
 var temperatureState = null;
 var lightState = null;
-
+*/
 const app = express();
 app.use(express.json());
 
@@ -80,7 +81,7 @@ app.get('/temperature', (req, res) =>  {
   
 });
 //Setup connessioni con i device
-
+/*
 thingShadows.on('connect', function() {
     thingShadows.register(name, {}, function() {
        thingShadows.get(name);
@@ -120,7 +121,7 @@ thingShadows.on('status',  function(name, stat, clientToken,  stateObject) {
   temperatureState = stateObject.state.reported.value.temperature;
   lightState = stateObject.state.reported.value.light;
 });
-
+*/
 const port = process.env.PORT || 5000;
 
 const addThing = async (thingNamePass) => {
@@ -178,16 +179,18 @@ const addThing = async (thingNamePass) => {
   fs.writeFileSync('./certStorage/' + thingNamePass + '/certificate.pem.crt', responseCreateCertAndKeys.certificatePem);
   console.log('certificati salvati');
   updateShadowInit(thingNamePass);
-  updateShadowForGazzosa(thingNamePass);
+  updateShadowForTesting(thingNamePass);
   //console.log(responsePublish);
+  // connectToAllShadows();
 
 };
+
 const updateShadowInit = async (thingNamePass) => {
   const config = {
     region: 'ap-south-1',
     credentials: {
-      accessKeyId: '',
-      secretAccessKey: '',
+      accessKeyId: secrets.accessKeyId,
+      secretAccessKey: secrets.secretAccessKey,
     },
   };
   const client = new IoTDataPlaneClient(config);
@@ -210,12 +213,12 @@ const updateShadowInit = async (thingNamePass) => {
   const responseUpdateShadow = await client.send(commandUpdateShadow);
   console.log("Shadow inizializzata");
 };
-const updateShadowForGazzosa = async (thingNamePass) => {
+const updateShadowForTesting= async (thingNamePass) => {
   const config = {
     region: 'ap-south-1',
     credentials: {
-      accessKeyId: '',
-      secretAccessKey: '',
+      accessKeyId: secrets.accessKeyId,
+      secretAccessKey: secrets.secretAccessKey,
     },
   };
   const client = new IoTDataPlaneClient(config);
@@ -239,6 +242,52 @@ const updateShadowForGazzosa = async (thingNamePass) => {
   console.log("Shadow aggiornata");
 };
 
+const connectToAllShadows =  () => {
+  const thingNames = [];
+  // get all subfolders names in certStorage in an array called things
+  const things = fs.readdirSync('./certStorage');
+  // for each subfolder in certStorage
+  things.forEach((thing) => {
+    //atdd the thing name to the array of thing names
+    if (thing !== 'GlobalCert') {
+      thingNames.push(thing);
+    }
+  });
+  console.log(thingNames);  
+  console.log(thingNames.length);
+  
+  //declare array of atwIot.thingShadow of size 
+  //atwIot.thingShadow is a class that allows to connect to a thing shadow
+  //the array will contain all the thing shadows
+  var thingShadows= [];
+  //connect to all the thing shadows
+  for (let i = 0; i < thingNames.length; i++) {
+    thingShadows[i] = new awsIot.thingShadow({
+      keyPath: './certStorage/' + thingNames[i] + '/privateKey.key',
+      certPath: './certStorage/' + thingNames[i] + '/certificate.pem.crt',
+      caPath: './certStorage/GlobalCert/AmazonRootCA1.pem',
+      clientId: thingNames[i],
+      host: 'a8bjo0oi9t9yw-ats.iot.ap-south-1.amazonaws.com',
+    });
+    thingShadows[i].on('connect', function() {
+      thingShadows[i].register(thingNames[i], {}, function () {
+        console.log('registered to thing shadow ' + thingNames[i]);
+        thingShadows[i].subscribe('$aws/things/'+thingNames[i]+'/shadow/update/documents');
+        });
+      });
+    //do for the on message
+    thingShadows[i].on('message', function (topic, payload) {
+      console.log('message', topic, payload.toString());
+    });
+    //read
+  }
+
+  // print content of thingShadows
+  
+  
+  
+};
+connectToAllShadows();
   
 
 
